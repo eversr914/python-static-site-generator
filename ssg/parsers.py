9ced5a25003b1f103a88f737acb072091bad6b23
Robert Evers <robert.evers9@gmail.com>
2020-06-18 10:35:52 -0700

    module2_v2

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -9,14 +9,14 @@ class Parser():
     def valid_extension(self, extension):
         return extension in self.extensions
 
-    def parse(self, path, source, dest):
+    def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
     def read(self, path):
         with open(path, "r") as file:
             return file.read()
 
-    def write(self, path, dest, content):
+    def write(self, path, dest, content, ext=".html"):
         full_path = self.dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
             file.write(content)
