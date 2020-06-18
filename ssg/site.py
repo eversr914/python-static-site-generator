1f6f3b70c317e55ea03ce43b2f2f0d9235ad334e
Robert Evers <robert.evers9@gmail.com>
2020-06-18 09:56:32 -0700

    final

--- ssg/site.py
+++ ssg/site.py
@@ -3,7 +3,7 @@ from pathlib import Path
 class Site ():
     def __init__(self, source, dest):
         self.source = Path(source)
-        selt.dest = Path(dest)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
